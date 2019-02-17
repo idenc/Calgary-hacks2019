f4dd75a572a791068a3098cf400680e745236d21
idenc <iden.craven@gmail.com>
2019-02-16 20:09:57 -0700

    Updated

--- main.py
+++ main.py
@@ -5,19 +5,40 @@ from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
 
-def _on_load_finished():
-    print("Url Loaded")
-
-
 class WebPage(QWebEngineView):
+    def on_load_finished(self):
+        self.page().runJavaScript(
+            "document.getElementsByClassName('LocationInputAutoComplete ui-autocomplete-input')[0].value = '" + self.stop_num + "';")
+        self.page().runJavaScript(
+            "document.getElementsByClassName('LocationInputAutoComplete ui-autocomplete-input')[0].focus();")
+        command = """
+        function eventFire(el, etype){
+          if (el.fireEvent) {
+            el.fireEvent('on' + etype);
+          } else {
+            var evObj = document.createEvent('Events');
+            evObj.initEvent(etype, true, false);
+            el.dispatchEvent(evObj);
+          }
+        };
+        eventFire(document.getElementById("TravelPlansMenuItem"), 'click');
+        document.getElementsByClassName('LocationInputAutoComplete ui-autocomplete-input')[0].focus();
+        eventFire(document.getElementById("TravelPlansMenuItem"), 'keydown');
+        eventFire(document.getElementById("TravelPlansMenuItem"), 'keypress');
+        """
+        self.page().runJavaScript(command)
+        print("Url Loaded")
+
     def __init__(self, bus, direction, stop_num):
         QWebEngineView.__init__(self)
-        self.current_url = ''
+        self.stop_num = stop_num
+        self.resize(300, 500)
+        self.settings().setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
+
         self.load(QUrl("http://hastinfoweb.calgarytransit.com/hastinfoweb2/NextDepartures?StopFilterIdentifier=" + bus +
                        "~~" + direction + "&StopFilterType=RouteDirection&StopIdentifier=" + stop_num + "&IsEmbedded"
                                                                                                         "=true"))
-        self.loadFinished.connect(_on_load_finished)
-        self.page().runJavaScript("map.zoom")
+        self.loadFinished.connect(self.on_load_finished)
 
 
 class MainWindow(QMainWindow):
@@ -29,20 +50,17 @@ class MainWindow(QMainWindow):
         self.show()
 
     def create_menu(self):
-        """ Creates the Main Menu """
-        self.main_menu = self.menuBar()
-        self.main_menu_actions = {}
-
-        self.file_menu = self.main_menu.addMenu("Example File Menu")
-        self.file_menu.addAction(QAction("Testing Testing", self))
+        return
 
     def add_web_widget(self):
         self.web_widget = WebPage("8", "North", "2350")
+
         self.setCentralWidget(self.web_widget)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setApplicationDisplayName("Calgary Transit Kiosk")
     main_window = MainWindow()
     main_window.showMaximized()
     sys.exit(app.exec_())  # only need one app, one running event loop
